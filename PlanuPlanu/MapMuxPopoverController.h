ce5beec3944da56b6f613f131a8b9546054eba31
Handsome Cam <handsomecam@semisafe.com>
2012-01-10 18:28:30 -0600

    Got better looking Mux popover working

--- PlanuPlanu/MapMuxPopoverController.h
+++ PlanuPlanu/MapMuxPopoverController.h
@@ -7,14 +7,16 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <PlanuKit/PlanuKit.h>
 
 @interface MapMuxPopoverController : NSViewController <NSTableViewDataSource, NSTableViewDelegate, NSPopoverDelegate>
 
 {
     NSArray* entities;
+    NuTurn* turn;
 }
 
 @property (nonatomic, retain) NSArray* entities;
-
+@property (nonatomic, retain) NuTurn* turn;
 
 @end
