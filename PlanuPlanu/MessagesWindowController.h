e53aecc69e72c41053a3a0761367d54d63942218
Handsome Cam <handsomecam@semisafe.com>
2012-01-12 16:20:57 -0600

    Added Messages window, incomplete but changing tack for a moment

---
+++ PlanuPlanu/MessagesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesWindowController.h
+//  PlanuPlanu
+//
+//  Created by Cameron Hotchkies on 1/12/12.
+//  Copyright (c) 2012 Roboboogie Studios. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MessagesWindowController : NSWindowController <NSOutlineViewDelegate, NSOutlineViewDataSource>
+
+@end
---
+++ PlanuPlanu/MessagesWindowController.m
@@ -0,0 +1,30 @@
+//
+//  MessagesWindowController.m
+//  PlanuPlanu
+//
+//  Created by Cameron Hotchkies on 1/12/12.
+//  Copyright (c) 2012 Roboboogie Studios. All rights reserved.
+//
+
+#import "MessagesWindowController.h"
+
+@implementation MessagesWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
