e054a76bb3b780af2e841c2d9303d11672ac75cf
Handsome Cam <handsomecam@semisafe.com>
2012-02-26 10:12:15 -0600

    Added Apache license notices to all PlanuPlanu source

--- PlanuPlanu/TurnScorePanelController.m
+++ PlanuPlanu/TurnScorePanelController.m
@@ -3,7 +3,19 @@
 //  PlanuPlanu
 //
 //  Created by Cameron Hotchkies on 2/4/12.
-//  Copyright (c) 2012 Roboboogie Studios. All rights reserved.
+//  Copyright 2012 Roboboogie Studios. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "TurnScorePanelController.h"
@@ -114,7 +126,7 @@
     {
         tickInterval = 2;
     }
-    if (maxPlanets > 100)
+    if (maxPlanets > 90)
     {
         tickInterval = 5;
     }
@@ -209,7 +221,7 @@
     {
         tickInterval = 2;
     }
-    if (maxShips > 100)
+    if (maxShips > 90)
     {
         tickInterval = 5;
     }
